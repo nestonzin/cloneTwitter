cf3c53da6b20b9b82b29eaf27e0c0957ea780129
nestonzin <dinestoor@outlook.com>
2022-01-26 16:42:17 -0300

    Primeiro commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Header, BackIcon, ProfileInfo } from '../Layout/styles';
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Neston</strong>
+          <span>18 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+
+      <ButtonMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </ButtonMenu>
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+ export const Container = styled.div`
+ display: flex;
+ flex-direction: column;
+
+ width: min(601px, 100%);
+ 
+ @media (min-width: 500px;) {
+   border-left: 1px solid var(--gray);
+   border-right: 1px solid var(--gray);
+
+
+ }
+
+ background-color: red;
+
+ `;
+
+ export const Header = styled.div`
+ position: sticky;
+ top: 0;
+ 
+ `;
+
+ export const BackIcon = styled.div``;
+
+ export const ProfileInfo = styled.div``;
